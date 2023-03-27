16049d0355ec18c1a26824e111e9a6520168adb5
ManjulaChalla <manjula.challa98@gmail.com>
2023-03-27 00:01:24 -0700

    Updated quasirandomGenerator

--- guided_quasirandomGenerator_SYCLMigration/build/02_sycl_migrated_optimized/CMakeFiles/run_gpu.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/run_gpu"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_gpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
